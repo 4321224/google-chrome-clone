c5e0060829ef5096963018bc07380bf637852319
Tanjung Anggraini <you@example.comkareen.anna2@gmail.com>
2023-01-08 19:29:48 +0700

    create index for home

--- src/components/HomeScreen/Home.js
+++ src/components/HomeScreen/Home.js
@@ -1,5 +1,29 @@
 import Header from "./Header";
+import React, { useState } from "react";
+import { recognition } from "../../api/VoiceSearch";
 function Home() {
+  const Home = ({ setSearch }) => {
+    const [term, setTerm] = useState("");
+
+    const [isVoiceSearch, setIsVoiceSearch] = useState(false);
+    const [voiceText, setVoiceText] = useState("");
+    const clearTerm = () => {
+      setTerm("");
+    };
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      if (
+        /^[a-zA-Z0-9].*/.test(term) ||
+        /^[a-zA-Z0-9]+[" "]/.test(term) ||
+        /^[" "]+[a-zA-Z0-9]/.test(term)
+      ) {
+        setSearch(term.trim());
+      }
+    };
+    const handleSearch = () => {
+        
+    }
+  };
   return (
     <div>
       <Header />
