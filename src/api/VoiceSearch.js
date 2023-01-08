f66757240cb32d6a9769f1e7062e678dbb2878c3
Tanjung Anggraini <you@example.comkareen.anna2@gmail.com>
2023-01-08 19:10:18 +0700

    voice search api

---
+++ src/api/VoiceSearch.js
@@ -0,0 +1,11 @@
+var SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+export const recognition = new SpeechRecognition();
+
+export const startReco = () => {
+  recognition.start();
+};
+export const stopReco = () => {
+  recognition.stop();
+};
