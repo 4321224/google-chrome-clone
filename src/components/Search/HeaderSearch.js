2ba2776b17da2b49d8273039dff67ff3d4d5a5fd
Tanjung Anggraini <you@example.comkareen.anna2@gmail.com>
2023-01-11 08:31:54 +0700

    header for search

--- src/components/Search/HeaderSearch.js
+++ src/components/Search/HeaderSearch.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { recognition } from "../../api/VoiceSearch";
 import VoiceSearchBox from "../HomeScreen/VoiceSearchBox";
 import "./HeaderSearch.css";
@@ -43,6 +44,64 @@ const HeaderSearch = ({ searchTerm, setSearch }) => {
       setSearch(term.trim());
     }
   };
+  return (
+    <>
+      {isVoiceSearch ? (
+        <VoiceSearchBox
+          voiceText={voiceText}
+          clearVoiceSearch={clearVoiceSearch}
+          openVoiceSearch={openVoiceSearch}
+        />
+      ) : null}
+      <div className="search-page-header">
+        <div className="col-md-12">
+          <nav className="navbar py-3">
+            <Link to="/" className="navbar-brand">
+              <img
+                src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+                alt="Google Logo"
+                height={50}
+                className="image-fluid ml-5"
+              />
+            </Link>
+            <div className="search-box col-md-5 border d-flex py-2 justify-content-between align-items-center">
+              <form className="form-search" onSubmit={(e) => handleSubmit(e)}>
+                <input
+                  type="text"
+                  name="term"
+                  id="term"
+                  value={term}
+                  onChange={(e) => setTerm(e.target.value)}
+                />
+              </form>
+              {term ? (
+                <i className="fa fa-close" onClick={() => clearTerm()}></i>
+              ) : (
+                ""
+              )}
+              <i
+                className="fa fa-microphone"
+                onClick={() => openVoiceSearch()}
+              ></i>
+              <i className="fa fa-search" onClick={() => handleSearch()}></i>
+            </div>
+            <ul className="nav ml-auto mr-5">
+              <li className="nav-item">
+                <a href="/" className="nav-link">
+                  <i className="fa fa-th"></i>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a href="/" className="nav-link">
+                  <i className="fa fa-user-circle-o"></i>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HeaderSearch;
